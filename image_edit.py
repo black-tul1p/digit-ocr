4b63f7ba909efdca3cb9f62918cad342d7d5a60c
black-tul1p <67044218+black-tul1p@users.noreply.github.com>
2021-05-28 12:05:45 +0400

    added remaining code

---
+++ image_edit.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from torchvision import torch
+import cv2
+
+def convert_bnw(in_path, out_path):
+	rgb_image = Image.open(in_path)
+	bnw_image = rgb_image.convert('L')
+	bnw_image.save(out_path)
+
+def resize_img(img_path):
+	input_im = cv2.imread(img_path)
+	input_im = cv2.resize(input_im, (28, 28))
+	cv2.imshow("image_converted", input_im)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+	return input_im
+
+def to_float_tensor(np_array):
+	return torch.from_numpy(np_array).float()
